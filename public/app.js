e83712539c6fc3dc91b67c72d0cd73a2d4880cde
haseeb <alamhaseeb036@gmail.com>
2020-07-24 20:35:36 +0500

    A SIMPLE TODO APP MADE BY ME

---
+++ public/app.js
@@ -0,0 +1,56 @@
+
+// CODED BY HASEEB ALAM RAFIQ
+
+alert("Welcome To Our TODO APP");
+alert("You Only Allow To ENTER 15 Characters.")
+function additem() { 
+    var a = document.getElementById("ip");
+    var aa = a.value.toUpperCase();
+    var text = document.createTextNode(aa);
+    var li = document.createElement("li");
+    li.setAttribute("id" , "li");
+    li.setAttribute("class" , "list-group-item list-group-item-dark");
+    li.appendChild(text);
+    var ul = document.getElementById("ul");
+    ul.appendChild(li);
+    a.value = "";
+
+    // delete button with text node:
+
+    var delbt = document.createElement("button");
+    var bttext = document.createTextNode(" "+ " " + " " + "DELETE");
+    delbt.appendChild(bttext);
+    li.appendChild(delbt);
+    delbt.setAttribute("class" , "delbt btn btn-danger fa fa-trash-o");
+    delbt.setAttribute("onclick" , "deleted(this)");
+
+
+    // edit button with text node:
+    var editbt = document.createElement("button");
+    var edittext = document.createTextNode(" "+ " " + " " +"EDIT");
+    editbt.appendChild(edittext);
+    li.appendChild(editbt);
+    editbt.setAttribute("class" , "editbt fa fa-pencil btn-success");
+    editbt.setAttribute("onclick" , "edit(this)");
+
+}
+
+function deleted(e) { 
+    e.parentNode.remove();
+}
+
+function edit(e) {
+    // var pro = prompt("WHAT YOU REPLCACE");
+    var pro = prompt("Change Item" , e.parentNode.firstChild.nodeValue);
+    e.parentNode.firstChild.nodeValue = pro; 
+}
+
+function deleall() {
+    var ul = document.getElementById("ul");
+    ul.innerHTML = "";
+}
+
+
+
+
+
